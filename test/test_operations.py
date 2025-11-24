d54297852aaa253e7dc55b1888fb5f44368b1a90
Elmahdi Bahadia <elmahdibahadia@gmail.com>
2025-11-24 17:09:04 -0500

    Fix tests for add and sub

--- test/test_operations.py
+++ test/test_operations.py
@@ -1,13 +1,12 @@
 from src.math_operations import add, sub
 
 def test_add():
-    assert add(5+2) == 7
-    assert add(5+5) == 10
-    assert add(4+4) == 8
-
+    assert add(5, 2) == 7
+    assert add(5, 5) == 10
+    assert add(4, 4) == 8
 
 def test_sub():
-    assert sub(-1,1) == -2
-    assert sub(7,3) == 4
-    assert sub(6,3) == 3
-    assert sub(9,3) == 6
+    assert sub(-1, 1) == -2
+    assert sub(7, 3) == 4
+    assert sub(6, 3) == 3
+    assert sub(9, 3) == 6

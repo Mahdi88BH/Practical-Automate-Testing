d5ba25e21b5d91b099cf748e0f7fa69ed02bfdd9
Elmahdi Bahadia <elmahdibahadia@gmail.com>
2025-11-24 16:19:27 -0500

    Second Commit

---
+++ test/test_operations.py
@@ -0,0 +1,13 @@
+from src.math_operations import add, sub
+
+def test_add():
+    assert add(5+2) == 7
+    assert add(5+5) == 10
+    assert add(4+4) == 8
+
+
+def test_sub():
+    assert sub(-1,1) == 0
+    assert sub(7,3) == 4
+    assert sub(6,3) == 3
+    assert sub(9,3) == 6
